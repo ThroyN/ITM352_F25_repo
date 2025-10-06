57d3eb5849cb3c9f212c65b576e44c8d3e629198
Throy Nicdao <nicdaot@hawaii.edu>
2025-10-05 20:13:38 -1000

    Initial commit

---
+++ Lab.Lists/Ex2a.py
@@ -0,0 +1,6 @@
+first_name = input("Enter first name: ")
+middle_name = input("Enter middle name: ")
+last_name = input("Enter last name: ")
+
+full_name = f"{first_name} {middle_name} {last_name}"
+print(full_name)
